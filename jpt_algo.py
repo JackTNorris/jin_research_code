14979f2771751c37aee9efc673d2d6edf87c2e75
Jack Norris <jacktimothynorris@gmail.com>
2022-11-15 13:13:05 -0600

    defined functions for deriving phase angle and mag from complex voltage

--- jpt_algo.py
+++ jpt_algo.py
@@ -1,19 +1,29 @@
 import cmath
 import math
+import numpy as np
 
 def jpt_algo(kMin1, kMin2, kMin3):
-    #to find the measurement at time k, take the 3 measurements prior (kMin1 ... KMin3) and plug it into the following to get
+    #to find the measurement at time k, take 
+    # the 3 measurements prior (kMin1 ... KMin3) and plug it into the following to get
     return 3 * kMin1 - 3 * kMin2 + kMin3
 
 #e^ix = cos x + i sin x
-def complex_voltage(magnitude, phase_angle):
-    #V = magnitude  * e^(j*(wt + -133.94034376773254))
+def calculate_complex_voltage(magnitude, phase_angle):
+    # V = magnitude  * e^(j*(wt + phase_angle))
+    # V = magnitude * cos(phase_angle) + magnitude * i * sin(phase_angle)
+    real_portion = math.cos(math.radians(phase_angle)) * magnitude
+    imaginary_portion = math.sin(math.radians(phase_angle)) * magnitude
+    voltage = complex(real_portion, imaginary_portion)
+    return voltage
 
-    voltage = complex(math.cos(phase_angle) * magnitude, math.sin(phase_angle) * magnitude) 
-    # voltage = magnitude * e
-
+def phase_angle_and_magnitude_from_complex_voltage(voltage):
+    phase_angle =  math.atan(voltage.image / voltage.real) #finds phase angle in radians
+    magnitude = voltage.real / math.cos(phase_angle)
+    return magnitude, phase_angle
 
 
 
 if __name__ == "__main__":
     jpt_algo(0,0,0)
+
+print("hello")
