e71686b8a8f7c408daecafcbfba0805e5fe2014e
Jack Norris <jacktimothynorris@gmail.com>
2022-11-16 09:45:22 -0600

    added ability to have PMU measurements

--- jpt_algo.py
+++ jpt_algo.py
@@ -2,6 +2,8 @@ import cmath
 import math
 import numpy as np
 
+wt = 0
+
 def jpt_algo(kMin1, kMin2, kMin3):
     #to find the measurement at time k, take 
     # the 3 measurements prior (kMin1 ... KMin3) and plug it into the following to get
@@ -10,20 +12,26 @@ def jpt_algo(kMin1, kMin2, kMin3):
 #e^ix = cos x + i sin x
 def calculate_complex_voltage(magnitude, phase_angle):
     # V = magnitude  * e^(j*(wt + phase_angle))
-    # V = magnitude * cos(phase_angle) + magnitude * i * sin(phase_angle)
-    real_portion = math.cos(math.radians(phase_angle)) * magnitude
-    imaginary_portion = math.sin(math.radians(phase_angle)) * magnitude
+    # V = magnitude * cos(wt + phase_angle) + magnitude * i * sin(wt + phase_angle)
+    real_portion = math.cos(math.radians(wt + phase_angle)) * magnitude
+    imaginary_portion = math.sin(math.radians(wt + phase_angle)) * magnitude
     voltage = complex(real_portion, imaginary_portion)
     return voltage
 
 def phase_angle_and_magnitude_from_complex_voltage(voltage):
-    phase_angle =  math.atan(voltage.image / voltage.real) #finds phase angle in radians
-    magnitude = voltage.real / math.cos(phase_angle)
-    return magnitude, phase_angle
+    phase_angle =  math.atan(voltage.image / voltage.real) - wt #finds phase angle in radians
+    magnitude = voltage.real / math.cos(phase_angle + wt)
+    return magnitude, math.degrees(phase_angle)
 
 
 
 if __name__ == "__main__":
-    jpt_algo(0,0,0)
+    # recent to least recent time measurements
+    pmu_measurements = [{"magnitude": 0, "phase_angle": 0}, {"magnitude": 0, "phase_angle": 0}, {"magnitude": 0, "phase_angle": 0}]
+    kMin = []
+    for num in range(0, 3):
+        kMin.insert(pmu_measurements[num]["magnitude"], pmu_measurements[num]["voltage"])
+    complex_voltage = jpt_algo(kMin[0],kMin[1],kMin[3])
+    print(phase_angle_and_magnitude_from_complex_voltage(complex_voltage))
 
 print("hello")
