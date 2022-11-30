2bacf49f699913dbfe9662dbebbed502485ef418
Jack Norris <jacktimothynorris@gmail.com>
2022-11-29 23:45:14 -0600

    got simple graph of actual vs predicted created

--- jpt_algo.py
+++ jpt_algo.py
@@ -1,8 +1,17 @@
-import cmath
 import math
+import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
+
 wt = 0
+
+def parse_csv_data():
+    data = pd.read_csv("pmu12.csv")
+    times = list(map(lambda time: time.replace("2014-01-28 23:00:", ""), data["TimeTag"].values))
+    magnitudes = [data["Magnitude01"].values, data["Magnitude02"].values, data["Magnitude03"].values]
+    phase_angles = [data["Angle01"].values, data["Angle02"].values, data["Angle03"].values]
+    return {"times": times, "magnitudes": magnitudes, "phase_angles": phase_angles}
 
 #jpt algorithm
 def jpt_algo(kMin1, kMin2, kMin3):
@@ -26,18 +35,33 @@ def phase_angle_and_magnitude_from_complex_voltage(voltage):
     magnitude = math.sqrt(voltage.real * voltage.real + voltage.imag * voltage.imag)
     return magnitude, math.degrees(phase_angle)
 
+def generate_jpt_predictions(magnitudes, phase_angles):
+    predictions = {"magnitudes": [], "phase_angles": []}
+    for i in range(0, len(magnitudes) - 3):
+        three_previous = []
+        for j in range(i, i + 3):
+            three_previous.append({"magnitude": magnitudes[j], "phase_angle": phase_angles[j]})
+        k_min = []
+        for measurement in three_previous:
+            complex_voltage = calculate_complex_voltage(measurement["magnitude"], measurement["phase_angle"])
+            k_min.append(complex_voltage)
+        complex_voltage_future_approximation = jpt_algo(k_min[0],k_min[1],k_min[2])
+        predicted_magnitude, predicted_phase_angle = phase_angle_and_magnitude_from_complex_voltage(complex_voltage_future_approximation)
+        predictions["magnitudes"].append(predicted_magnitude)
+        predictions["phase_angles"].append(predicted_phase_angle)
+    return predictions
+
 
 
 if __name__ == "__main__":
-    # recent to least recent time measurements
-    pmu_measurements = [
-        {"magnitude": 253829.86075, "phase_angle": -13.9343335775816},
-        {"magnitude": 253811.55021, "phase_angle": -14.1348688058774},
-        {"magnitude": 253793.23967, "phase_angle": -14.3525927680271}
-    ]
-    kMin = []
-    for pmu_measurement in pmu_measurements:
-        complex_voltage = calculate_complex_voltage(pmu_measurement["magnitude"], pmu_measurement["phase_angle"])
-        kMin.append(complex_voltage)
-    complex_voltage_future_approximation = jpt_algo(kMin[0],kMin[1],kMin[2])
-    print(phase_angle_and_magnitude_from_complex_voltage(complex_voltage_future_approximation))
+    pmu_raw_data = parse_csv_data()
+    y1 = pmu_raw_data["magnitudes"][0][3:]
+    x = pmu_raw_data["times"][3:]
+    fig, ax = plt.subplots()
+    ax.plot(x, y1, color="g", label="actual")
+
+    y2 = generate_jpt_predictions(pmu_raw_data["magnitudes"][0], pmu_raw_data["phase_angles"][0])["magnitudes"]
+
+
+    ax.plot(x, y2, color="r", label="predicted")
+    plt.show()
